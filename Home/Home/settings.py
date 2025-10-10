82eb171a417f67689e2ec19dde76bc592d510d15
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2025-10-10 08:08:08 -0700

    added home_auth folder

--- Home/Home/settings.py
+++ Home/Home/settings.py
@@ -29,7 +29,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'school',
-    'student'
+    'student',
+    'home_auth'
 ]
 
 MIDDLEWARE = [
--- Home/Home/urls.py
+++ Home/Home/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("school.urls")),
-    path('student/',include("student.urls"))
+    path('student/',include("student.urls")),
+    path('authentication/',include("home_auth.urls"))
 
 ]
